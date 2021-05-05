27e39dfea55d4057b1c6d73dfbb4aa8cf745b74b
yvoste <yves.mitaine@nordnet.fr>
2021-05-05 19:38:58 +0200

    premier commit

---
+++ src/services/auth-header.js
@@ -0,0 +1,25 @@
+/*
+export default function authHeader() {
+    let user = JSON.parse(localStorage.getItem('user'));
+  
+    if (user && user.accessToken) {
+      return { Authorization: 'Bearer ' + user.accessToken };
+    } else {
+      return {};
+    }
+}
+  */
+
+//Note: For Node.js Express back-end, please use x-access-token header like this:
+
+export default function authHeader() {
+  let user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.accessToken) {
+    // for Node.js Express back-end
+    return { 'x-access-token': user.accessToken };
+  } else {
+    return {};
+  }
+}
+
